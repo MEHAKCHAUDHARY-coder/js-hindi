81b52d8fcbc7be83b48aa162b6ae0af25baf7d55
MEHAKCHAUDHARY-coder <153123998+MEHAKCHAUDHARY-coder@users.noreply.github.com>
2023-12-19 07:24:24 +0000

    container datatypes config

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use Strict"      //treat all JS code as newer version
+
+// alert(3+3)     //we are using node js not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number  => 2 to power 53
+// BigInt
+// string => ""
+// boolean =? true/false
+// null =>standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null);         // object
+
+console.log(typeof undefined);   // undefined
