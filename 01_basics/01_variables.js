fc1261d0aa726dbba3966b89131a403fc53bf81c
MEHAKCHAUDHARY-coder <153123998+MEHAKCHAUDHARY-coder@users.noreply.github.com>
2023-12-19 07:23:27 +0000

    container variables config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+console.log("hijjj");
+const accountId = 22
+var accountEmail = "mehak@gmail.com"  
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+let accountPassword  = "23334"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 5
+
+accountEmail = "baby@gmail.com"
+accountPassword = "5545"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountState]);
